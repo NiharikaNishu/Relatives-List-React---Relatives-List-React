b0c891a9d0374dda9a97efebc62342de3665c69b
Niharika Nishu <46600925+NiharikaNishu@users.noreply.github.com>
2020-10-18 23:43:37 +0530

    Code changed

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,17 @@ import '../styles/App.css';
 
 class App extends Component {
     render() {
-
+     const relatives = ["S, "n", "m"];
         return(
            <>
 //write your code here
+    <ol key = "relativeList">
+        {
+            relatives.map((relatives,index) => (
+                <li key ={`relativeListItem${index + 1}`}>{relatives}</li>
+            ))
+        }
+    </ol>
 	</>
         )
     }
